2c0951d8bfedd44ffe9078b4ef053166528c4859
Devin Don <I.INF@Outlook.com>
2020-07-25 16:56:51 +0000

    feat(model): add motto model (interface)

---
+++ src/main/motto/motto.model.ts
@@ -0,0 +1,19 @@
+/** 格言 */
+export interface Motto {
+
+  /** 编号 */
+  id: number;
+
+  /** 作者 */
+  author?: string;
+
+  /** 内容 */
+  content: string;
+
+  /** 发布日期 */
+  date: number;
+
+  /** 点赞 */
+  like: number;
+
+}
