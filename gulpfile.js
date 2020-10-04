ce2e7b3ff7e6279dfbafc5fb5323a30311f73769
Devin Don <I.INF@Outlook.com>
2020-10-04 23:19:42 +0800

    chore(docker): add docker file & gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const gulp = require('gulp');
-const gulpClean = require('gulp-clean');
-const gulpConcat = require('gulp-concat');
 const { logger } = require('@iinfinity/logger');
+const { execSync } = require('child_process');
 
 const read = gulp.src;
 const save = gulp.dest;
 
 const task = process.argv[2];
 const arch = process.argv[3];
+const name = require('./package.json').name;
+const registry = 'registry.don.red';
+
 logger.log(
   '\x1B[36m%s\x1B[0m',
   `
@@ -39,39 +41,24 @@ function catchError(err) {
 }
 
 /** Clean all dist folder. */
-async function clean() {
-  read(
-    [
-      'client/dist',
-      'server/dist',
-      'dist'
-    ],
-    { allowEmpty: true }
-  )
-    .pipe(gulpClean());
+function clean() {
+  // read(
+  //   [
+  //     'client/dist',
+  //     'server/dist',
+  //     'dist'
+  //   ],
+  //   { allowEmpty: true }
+  // )
+  //   .pipe(gulpClean());
+  execSync('npm run clean');
 }
 
 /** Build and pack application. */
-async function pack(arch) {
-  const suffix = arch ? `.${arch}` : '';
-  // read(`docker/config/**/*`)
-  //   .pipe(save('dist/config'));
-  // read(`docker/server/Dockerfile${suffix}`)
-  //   .on('error', catchError)
-  //   .pipe(gulpConcat('Dockerfile'))
-  //   .pipe(save('dist/server'));
-  // read(`docker/docker-compose${suffix}.yml`)
-  //   .on('error', catchError)
-  //   .pipe(gulpConcat('docker-compose.yml'))
-  //   .pipe(save('dist'));
-  read('docker/**/*').pipe(save('dist'));
-  read('client/dist/**/*').pipe(save('dist/client'));
-  read([
-    'server/dist/**/*.js',
-    'server/package*.json',
-    'server/rester.json'
-  ], { allowEmpty: true }
-  ).pipe(save('dist/server'));
+function pack(arch) {
+  execSync('npm run pack');
+  execSync(`docker build -t ${registry}/${name} .`);
+  execSync(`docker push ${registry}/${name}`);
 }
 
 switch (task.toLowerCase()) {
