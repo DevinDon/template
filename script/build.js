f4d51326055074fe9c254ad865b0e5243d65afb8
Devin Don <I.INF@Outlook.com>
2020-10-04 23:48:54 +0800

    fix(docker): fix docker file

--- script/build.js
+++ script/build.js
@@ -4,7 +4,7 @@ function build() {
   const { execSync } = require('child_process');
   const name = require('../package.json').name;
   const registry = 'registry.don.red';
-  console.log(execSync(`docker build -t ${registry}/${name} ./script`).toString());
+  console.log(execSync(`docker build -t ${registry}/${name} .`).toString());
   console.log(execSync(`docker push ${registry}/${name}`).toString());
 }
 
