43e24db6109c0943522e0c0f623f89a2774657c5
Devin Don <i.inf@outlook.com>
2021-03-23 13:13:21 +0800

    perf(handlers): optimize access handler

--- src/main/common/handlers/access.handler.ts
+++ src/main/common/handlers/access.handler.ts
@@ -6,21 +6,21 @@ export class AccessHandler extends BaseHandler {
   async handle(next: () => Promise<any>): Promise<any> {
     const result = await next();
 
-    const ips = parseIPsFromRequest(this.request);
+    const access = {
+      method: this.request.method?.toUpperCase(),
+      path: this.mapping?.path ?? this.request.url,
+      query: JSON.stringify(this.mapping?.queryObject),
+      headers: this.request.headers,
+      timestamp: new Date(),
+      ips: parseIPsFromRequest(this.request),
+      version: this.request.httpVersion,
+      statusCode: this.response.statusCode,
+      statusMessage: this.response.statusMessage,
+      length: result ? result.length : 0,
+    };
 
     AccessEntity
-      .insert({
-        method: this.request.method?.toUpperCase(),
-        path: this.mapping?.path ?? this.request.url,
-        query: JSON.stringify(this.mapping?.queryObject),
-        headers: this.request.headers,
-        timestamp: new Date(),
-        ips,
-        version: this.request.httpVersion,
-        statusCode: this.response.statusCode,
-        statusMessage: this.response.statusMessage,
-        length: result ? result.length : 0,
-      })
+      .insert(access)
       .catch(error => this.rester.logger.warn(`Record log failed: ${error}`));
 
     return result;
