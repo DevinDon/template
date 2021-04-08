56c8d1396b2253baacde3b787d762f5d40be716b
Devin Don <i.inf@outlook.com>
2021-04-08 11:30:31 +0800

    chore(config): update config

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { getLogger } from 'https://deno.land/std@0.92.0/log/mod.ts';
+
 export interface User {
   name: string;
   age: number;
@@ -6,3 +8,5 @@ export interface User {
 export const greeting = ({ name, age }: User) => `Hello, ${name}! You are ${age} years old.`;
 
 export const isAdult = ({ age }: User) => age >= 18;
+
+getLogger().info(greeting({ name: 'Deno', age: 2 }));
